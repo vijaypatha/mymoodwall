c69333dee7a65141a8d77a82825d0418e9bbb159
vijaypatha <elderpatha@gmail.com>
2016-07-30 20:49:26 -0600

    mm

--- public/views/mormon/mormonServices.js
+++ public/views/mormon/mormonServices.js
@@ -5,7 +5,7 @@ angular
  this.getDataFromServer = function(search){
   search = " i'm a mormon + living + prophet + mormon channel";
   var baseUrl = 'https://www.googleapis.com/youtube/v3';
-   return $http.get(baseUrl+'/search?part=snippet&key=apiKey&maxResults=7&q='+search)
+   return $http.get(baseUrl+'/search?part=snippet&key=AIzaSyAqZmMtSJor3zfR5fsXEylA0TEZt_0KmTw&maxResults=7&q='+search)
    .then(function(response){
       console.log(response);
       return response.data.items.map(function(item){
